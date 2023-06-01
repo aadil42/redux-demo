86d1b9032fc83a2c27c533e8d95b7708554dc7bc
aadil42 <adilkelawala@gmail.com>
2023-06-01 14:27:41 +0530

    dispatching action to update the state for redux

--- redux-demo.js
+++ redux-demo.js
@@ -3,9 +3,17 @@ const redux = require('redux');
 // the  reducer function responsible for changing the state.
 // it takes two arguments state and action
 const counterReducer = (state = {counter: 0}, action) => {
-    return {
-        counter : state.counter + 1
+    if(action.type === 'increment') {
+        return {
+            counter: state.counter + 1
+        }
     }
+    if(action.type === 'decrement') {
+        return {
+            counter: state.counter - 1
+        }
+    }
+    return state;
 }
 
 // creating the store. this also takes reducer function which is responsible for chaging state.
@@ -17,6 +25,11 @@ const mainComponent = () => {
 }
 
 store.subscribe(mainComponent);
+// store.dispatch({type: 'increment'});
+// store.dispatch({type: 'increment'});
+// store.dispatch({type: 'increment'});
+// store.dispatch({type: 'increment'});
+// store.dispatch({type: 'decrement'});
 
 
 
